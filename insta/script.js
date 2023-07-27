6d8788c91751a3687712b0f01e6fa1927610b632
Akash <akashghode1234@gmail.com>
2023-07-27 14:04:23 +0530

    scrip changes

--- insta/script.js
+++ insta/script.js
@@ -1,15 +1,43 @@
 // follow or following
-function toggleFollow() {
-    const followButtons = document.querySelectorAll(".follow");
+// function toggleFollow() {
+//     const followButtons = document.getElementsByClassName("follow");
 
-    followButtons.forEach(button => {
-        if (button.textContent === "Follow") {
-            button.textContent = "Following";
-        } else if (button.textContent === "Following") {
-            button.textContent = "Follow";
-        }
-    });
+//     followButtons.forEach(button => {
+//         if (followButtons.textContent === "Follow") {
+//           followButtons.textContent = "Following";
+//         } else if (followButtons.textContent === "Following") {
+//           followButtons.textContent = "Follow";
+//         }
+//     });
+// }
+
+
+// Function to toggle the follow button text
+function toggleFollow(button) {
+  if (button.textContent === "Follow") {
+    button.textContent = "Following";
+  } else if (button.textContent === "Following") {
+    button.textContent = "Follow";
+  }
 }
+
+// Get all elements with class "follow"
+const followButtons = document.querySelectorAll(".follow");
+
+// Attach a click event listener to each button
+followButtons.forEach(button => {
+  button.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent any default behavior of the button (if it's inside a form, etc.)
+    toggleFollow(this); // Pass the clicked button to the toggleFollow function
+  });
+});
+
+
+
+
+
+
+
 // like button
 
 function toggleLike(button) {
